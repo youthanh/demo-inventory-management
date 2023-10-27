10a9eea49d31f45e552d740e8e1862b71b6e431d
youthanh <youchithanh1@gmail.com>
2023-10-27 11:44:11 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return view('welcome');
-    return view('index');
+    return view('welcome');
+    // return view('index');
 });
