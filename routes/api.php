794d189dcfdd649e1fb38d5a0738543c96c05952
yct <youchithanh1@gmail.com>
2023-11-01 00:41:39 +0700

    products

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< Updated upstream
-use App\Http\Controllers\Authentication;
-=======
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\ProductController;
->>>>>>> Stashed changes
 
 /*
 |--------------------------------------------------------------------------
@@ -24,13 +20,6 @@ use App\Http\Controllers\ProductController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-<<<<<<< Updated upstream
-Route::middleware('auth:sanctum')->delete('/logout', [Authentication::class, 'logout']);
-
-Route::post('/login', [Authentication::class, 'login']);
-Route::post('/register', [Authentication::class, 'register']);
-=======
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->delete('/logout', [AuthController::class, 'logout']);
@@ -39,4 +28,3 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::resource('product-categories', ProductCategoryController::class);
     Route::resource('products', ProductController::class);
 });
->>>>>>> Stashed changes
