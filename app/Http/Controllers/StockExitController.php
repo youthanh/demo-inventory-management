4da91d172ac786aeaa9b712d9723ce0ad5e4a8d8
yct <youchithanh1@gmail.com>
2023-11-04 09:38:45 +0700

    Update StockExitController.php

--- app/Http/Controllers/StockExitController.php
+++ app/Http/Controllers/StockExitController.php
@@ -22,7 +22,11 @@ class StockExitController extends Controller
 
     public function index()
     {
-        //
+        $model = new StockExit;
+        $query = $model->query();
+        $this->applyQuery($query, $model);
+        $result = $query->with('warehouse')->simplePaginate(request('per_page', null));
+        return response()->json($result, 200);
     }
 
     /**
@@ -59,7 +63,12 @@ class StockExitController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $model = StockExit::with('warehouse')->with('items')->find($id);
+        if (!$model) {
+            return response()->json(['message' => 'Phiếu không tồn tại'], 404);
+        }
+
+        return response()->json($model, 200);
     }
 
     /**
@@ -67,7 +76,36 @@ class StockExitController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $stockExit = stockExit::find($id);
+        if (!$stockExit) {
+            return response()->json(['message' => 'Phiếu không tồn tại'], 404);
+        }
+        if ($stockExit->confirmed) {
+            return response()->json(['message' => 'Phiếu đã duyệt. Không thể sửa!'], 422);
+        }
+
+        // Validate
+        $batch = new Batch;
+        $validate = $this->applyValidate($stockExit->validate, ['name' => 'items', 'validate' => $batch->validate], 'update', ['id' => $id, 'fields' => ['code']]);
+        $request->validate($validate);
+
+        $isSuccess = $stockExit->update($request->all());
+        if ($isSuccess) {
+            $stockExitItemsData = $request->input('items', []);
+            if (!empty($stockExitItemsData)) {
+                $stockExit->items()->delete();
+                foreach ($stockExitItemsData as $itemData) {
+                    $itemData['stock_exit_id'] = $stockExit->id;
+                    $itemData['warehouse_id'] = $stockExit->warehouse_id;
+                    $itemData['date'] = $stockExit->date;
+
+                    $batch->create($itemData);
+                }
+            }
+            return response()->json(['message' => 'Lưu thành công', 'data' => $stockExit], 200);
+        }
+
+        return response()->json(['message' => 'Lưu thất bại'], 500);
     }
 
     /**
@@ -75,6 +113,17 @@ class StockExitController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $stockExit = StockExit::find($id);
+        if (!$stockExit) {
+            return response()->json(['message' => 'Phiếu không tồn tại'], 404);
+        }
+        if ($stockExit->confirmed) {
+            return response()->json(['message' => 'Phiếu đã duyệt. Không thể xóa!'], 422);
+        }
+
+        $stockExit->items()->delete();
+        $stockExit->delete();
+
+        return response()->json(['message' => 'Xóa thành công']);
     }
 }
